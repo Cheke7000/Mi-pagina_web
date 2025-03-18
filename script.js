edc37e075c2f3b8b8e82317aa70f505fdfd52d68
Cheke7000 <armando.ezequiel7000@gmail.com>
2025-03-18 07:25:16 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-// Modo Oscuro
-const btnModoOscuro = document.getElementById("modoOscuro");
-btnModoOscuro.addEventListener("click", () => {
-    document.body.classList.toggle("modo-oscuro");
-});
+const botonModoOscuro = document.getElementById("modoOscuro");
+const body = document.body;
 
-// Botón para subir
-const btnSubir = document.getElementById("subir");
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 200) {
-        btnSubir.style.display = "block";
-    } else {
-        btnSubir.style.display = "none";
-    }
-});
-
-btnSubir.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+botonModoOscuro.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    botonModoOscuro.textContent = body.classList.contains("dark-mode") ? "☀️" : "🌙";
 });
